0e26b57ddf5244353b2d0fc8b89a931511b636f2
Rizhi-KotE <d1mqn1ch@mail.ru>
2018-03-17 11:18:53 +0300

    nice version

--- client/js/migrate-to-module/run-all.sh
+++ client/js/migrate-to-module/run-all.sh
@@ -3,3 +3,5 @@
 
 find $1 -name *.temp.js -type f -exec rm {} \;
 find $1 -name *.js -type f -exec ./replace.sh {} {}.temp.js \;
+./replace-imports.sh $1
+find $1 -name *.js ! -name *.temp.js -type f -exec mv {}.temp.js {} \;
