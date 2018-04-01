ee1137c4c0549da4fc4816ae48754134b6a0cd9e
Rizhi-KotE <d1mqn1ch@mail.ru>
2018-04-01 22:12:23 +0300

    split sc-web

--- client/js/index.js
+++ client/js/index.js
@@ -1,16 +1,32 @@
 import "./startup";
 
-import * as Core from "./Core/index";
-import * as Ui from "./Ui/index";
-import * as Utils from  "./Utils/index";
+import * as Core from "core";
+import * as Ui from "ui";
+import * as Utils from "utils";
+import * as R from "ramda";
 export const core = Core;
 export const ui = Ui;
 export const utils = Utils;
 
-import * as sctp from "./Utils/sctp"
-import {TripleUtils} from "./Utils/TripleUtils"
-import * as scTypes from "./Utils/ScTypes"
+// Export legacy global objects
+const keys = [
+    'SctpCommandType',
+    'SctpResultCode',
+    'SctpIteratorType',
+    'SctpEventType',
+    'String2ArrayBuffer',
+    'ArrayBuffer2String',
+    'SctpConstrIter',
+    'SctpClient',
+    'SctpClientCreate',
+    'TripleUtils'
+];
 
-Object.assign(window, sctp, scTypes);
-window.TripleUtils = TripleUtils;
+const scTypeRegExp = /^sc_type.*/;
+const scTypesKeys = R.filter(scTypeRegExp.test.bind(scTypeRegExp), R.keys(utils));
 
+const legacyKeys = scTypesKeys.concat(keys);
+const legacySelectors = R.map(R.prop, legacyKeys);
+const legacyValues = R.juxt(legacySelectors)(utils);
+const legacy = R.zipObj(scTypesKeys, legacyValues);
+Object.assign(window, legacy);
