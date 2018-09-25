dbd984edc40aa8187ce8fddcbfa0f827ac4880bf
Max <stelmachenokm@gmail.com>
2018-09-25 21:50:31 +0300

    show/hide div by checkbox

--- client/js/Ui/RemoveElements.js
+++ client/js/Ui/RemoveElements.js
@@ -10,12 +10,151 @@ $(document).ready(function () {
         } else {
             document.getElementsByClassName("mode-switching-panel")[0].style.display = "none";
         }
-        var div = document.getElementsByClassName('scs-scn-field-root')[0];
-        if (div != null){
-            div.remove();
+
+
+        //идентификатор*
+        if (c.checked) {
+            document.querySelectorAll('div > div > a[sc_addr="262144"]').forEach(function (element) {
+                element.parentNode.parentNode.style.display = "";
+                var nextElem = element.parentNode.parentNode.nextSibling;
+                while (!nextElem.classList.contains("scs-scn-field")) {
+                    nextElem.style.display = "";
+                    nextElem = nextElem.nextSibling;
+                }
+            });
+        } else {
+            document.querySelectorAll('div > div > a[sc_addr="262144"]').forEach(function (element) {
+                element.parentNode.parentNode.style.display = "none";
+                var nextElem = element.parentNode.parentNode.nextSibling;
+                while (!nextElem.classList.contains("scs-scn-field")) {
+                    nextElem.style.display = "none";
+                    nextElem = nextElem.nextSibling;
+                }
+            });
         }
 
-        window.SCsViewer(window.sandbox).updateComponent();
+
+        //системный идентификатор*
+        if (c.checked) {
+            document.querySelectorAll('div > div > a[sc_addr="196608"]').forEach(function (element) {
+                element.parentNode.parentNode.style.display = "";
+            });
+        } else {
+            document.querySelectorAll('div > div > a[sc_addr="196608"]').forEach(function (element) {
+                element.parentNode.parentNode.style.display = "none";
+            });
+        }
+
+
+        //трансляция sc-текста*
+        if (c.checked) {
+            document.querySelectorAll('div > a[sc_addr="212271104"]').forEach(function (element) {
+                var afterKeyScElem = element.parentNode.nextSibling.nextSibling;
+                afterKeyScElem.childNodes[0].style.display = "";
+                afterKeyScElem.childNodes[1].childNodes[0].style.display = "";
+                afterKeyScElem.childNodes[1].childNodes[1].style.display = "";
+                afterKeyScElem.childNodes[1].childNodes[2].childNodes[0].style.display = "";
+                afterKeyScElem.childNodes[1].childNodes[2].childNodes[1].childNodes[0].style.display = "";
+                afterKeyScElem.childNodes[1].childNodes[2].childNodes[1].childNodes[1].style.display = "";
+            });
+        } else {
+            document.querySelectorAll('div > a[sc_addr="212271104"]').forEach(function (element) {
+                var afterKeyScElem = element.parentNode.nextSibling.nextSibling;
+                afterKeyScElem.childNodes[0].style.display = "none";
+                afterKeyScElem.childNodes[1].childNodes[0].style.display = "none";
+                afterKeyScElem.childNodes[1].childNodes[1].style.display = "none";
+                afterKeyScElem.childNodes[1].childNodes[2].childNodes[0].style.display = "none";
+                afterKeyScElem.childNodes[1].childNodes[2].childNodes[1].childNodes[0].style.display = "none";
+                afterKeyScElem.childNodes[1].childNodes[2].childNodes[1].childNodes[1].style.display = "none";
+            });
+        }
+
+        var select = document.getElementById("language-select");
+        var selectedLangId = select.options[select.selectedIndex].getAttribute("sc_addr");
+        var notSelectedIds = [];
+
+        for(var i = 0; i < select.length; i++){
+                if (!select.options[i].getAttribute("sc_addr").includes(selectedLangId)){
+                    notSelectedIds.push(select.options[i].getAttribute("sc_addr"));
+            }
+        }
+
+        document.querySelectorAll('div > a[sc_addr="8650752"]').forEach(function (element) {
+            var langScAddr = element.parentNode.nextSibling.nextSibling.childNodes[1].childNodes[1].childNodes[0].getAttribute("sc_addr");
+            if (notSelectedIds.includes(langScAddr)){
+                element.parentNode.nextSibling.style.display = "none";
+                element.parentNode.nextSibling.nextSibling.style.display = "none";
+            }
+        });
+
+
+
+        //основной идентификатор* на другом языке
+        if (c.checked) {
+            document.querySelectorAll('div > a[sc_addr="8650752"]').forEach(function (element) {
+                var langScAddr = element.parentNode.nextSibling.nextSibling.childNodes[1].childNodes[1].childNodes[0].getAttribute("sc_addr");
+                if (notSelectedIds.includes(langScAddr)){
+                    element.parentNode.nextSibling.style.display = "";
+                    element.parentNode.nextSibling.nextSibling.style.display = "";
+                }
+            });
+        } else {
+            document.querySelectorAll('div > a[sc_addr="8650752"]').forEach(function (element) {
+                var langScAddr = element.parentNode.nextSibling.nextSibling.childNodes[1].childNodes[1].childNodes[0].getAttribute("sc_addr");
+                if (notSelectedIds.includes(langScAddr)){
+                    element.parentNode.nextSibling.style.display = "none";
+                    element.parentNode.nextSibling.nextSibling.style.display = "none";
+                }
+            });
+        }
+
+        if (c.checked) {
+            document.querySelectorAll('div > a[sc_addr="8650752"]').forEach(function (element) {
+                var nextElem = element.parentNode.parentNode.nextSibling;
+                while (nextElem.getAttribute("style") == null || !nextElem.getAttribute("style").includes("padding-left: 40px")) {
+                    nextElem = nextElem.nextSibling;
+                }
+                nextElem.childNodes.forEach(function (element) {
+                    if (element.getAttribute("class") != null && element.getAttribute("class").includes("scs-scn-field")) {
+                        element.childNodes.forEach(function (element) {
+                            if (element.getAttribute("style") != null && element.getAttribute("style").includes("padding-left: 20px")) {
+                                element.childNodes.forEach(function (element) {
+                                    if (notSelectedIds.indexOf(element.getAttribute("sc_addr")) !== -1) {
+                                        nextElem.style.display = "";
+                                        nextElem.previousSibling.childNodes.forEach(function (element) {
+                                            element.style.display = "";
+                                        });
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+            });
+        } else {
+            document.querySelectorAll('div > a[sc_addr="8650752"]').forEach(function (element) {
+                var nextElem = element.parentNode.parentNode.nextSibling;
+                while (nextElem.getAttribute("style") == null || !nextElem.getAttribute("style").includes("padding-left: 40px")) {
+                    nextElem = nextElem.nextSibling;
+                }
+                nextElem.childNodes.forEach(function (element) {
+                    if (element.getAttribute("class") != null && element.getAttribute("class").includes("scs-scn-field")) {
+                        element.childNodes.forEach(function (element) {
+                            if (element.getAttribute("style") != null && element.getAttribute("style").includes("padding-left: 20px")) {
+                                element.childNodes.forEach(function (element) {
+                                    if (notSelectedIds.indexOf(element.getAttribute("sc_addr")) !== -1) {
+                                        nextElem.style.display = "none";
+                                        nextElem.previousSibling.childNodes.forEach(function (element) {
+                                            element.style.display = "none";
+                                        });
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+            });
+        }
     };
     if (c.checked) {
         document.getElementsByClassName("mode-switching-panel")[0].style.display = "";
@@ -27,95 +166,7 @@ $(document).ready(function () {
 // button.onclick = getElementsForRemove(cont_idtf);
 export async function getElementsForRemove(addr) {
 
-    //checkbox allow all scn constructions
-    if (c.checked) return [];
+    return [];
 
-    var for_remove = []; //удаляемые элементы
-    var cont = addr;
-    //заносим в множестов for_remove отношение nrel_system_identifier
-    for_remove.push(scKeynodes.nrel_system_identifier);
-    try {
-        var systemIdtfConstrs = await new Promise((success, faild) => window.sctpClient.iterate_constr(
-            //итератор для элементов контура
-            SctpConstrIter(SctpIteratorType.SCTP_ITERATOR_3F_A_A,
-                [cont,
-                    sc_type_arc_pos_const_perm,
-                    0
-                ], {
-                    "contour_elem": 2
-                }),
-            //итератор для поиска nrel_system_identifier
-            SctpConstrIter(SctpIteratorType.SCTP_ITERATOR_5F_A_A_A_F,
-                ["contour_elem",
-                    sc_type_arc_common | sc_type_const,
-                    0,
-                    sc_type_arc_pos_const_perm,
-                    window.scKeynodes.nrel_system_identifier
-                ], {
-                    "idtf_arc": 1,
-                    "idtf_node": 2,
-                    "idtf_relation": 3
-                })
-        ).then(success, faild));
-        for (let idx in systemIdtfConstrs.results) {
-            for_remove.push(systemIdtfConstrs.get(idx, "idtf_arc"));
-            for_remove.push(systemIdtfConstrs.get(idx, "idtf_node"));
-            for_remove.push(systemIdtfConstrs.get(idx, "idtf_relation"));
-        }
-    } catch (e) {
-        console.error("Cannot find system_idtf in contour", e);
-    }
-
-    //текущий язык
-    var current_lang = SCWeb.core.ComponentSandbox.prototype.getCurrentLanguage();
-
-    try {
-        //итератор для поиска nrel_main_idtf
-        var mainIdtfConstr = await
-            new Promise((success, faild) => window.sctpClient.iterate_constr(
-                SctpConstrIter(SctpIteratorType.SCTP_ITERATOR_3F_A_A,
-                    [cont,
-                        sc_type_arc_pos_const_perm,
-                        0
-                    ], {"contour_elem": 2}),
-                SctpConstrIter(SctpIteratorType.SCTP_ITERATOR_5F_A_A_A_F,
-                    ["contour_elem",
-                        sc_type_arc_common | sc_type_const,
-                        0,
-                        sc_type_arc_pos_const_perm,
-                        window.scKeynodes.nrel_main_idtf
-                    ], {
-                        "idtf_arc": 1,
-                        "idtf_node": 2,
-                        "idtf_relation": 3
-                    }),
-                SctpConstrIter(SctpIteratorType.SCTP_ITERATOR_3A_A_F,
-                    [0,
-                        sc_type_arc_pos_const_perm,
-                        "idtf_node"
-                    ], {
-                        "language": 0,
-                        "language_arc": 1
-                    }),
-                SctpConstrIter(SctpIteratorType.SCTP_ITERATOR_3F_A_F,
-                    [window.scKeynodes.languages,
-                        sc_type_arc_pos_const_perm,
-                        "language"
-                    ])
-            ).then(success, faild));
-
-        for (let idx in mainIdtfConstr.results) {
-            if (mainIdtfConstr.get(idx, "language") !== current_lang) {
-                for_remove.push(mainIdtfConstr.get(idx, "idtf_arc"));
-                for_remove.push(mainIdtfConstr.get(idx, "idtf_node"));
-                for_remove.push(mainIdtfConstr.get(idx, "idtf_relation"));
-                for_remove.push(mainIdtfConstr.get(idx, "language"));
-                for_remove.push(mainIdtfConstr.get(idx, "language_arc"));
-            }
-        }
-    } catch (e) {
-        console.error("Cannot find main_idtf in contour", e);
-    }
-    return for_remove;
 
 }
