70555368d271fd5e247d1fba4dcec359a17882fa
DBrutski <d1mqn1ch@gmail.com>
2019-01-13 07:12:57 +0300

    #136: Calculate boundedRectangle of scg elements representation

--- components/scg/src/scg-math.js
+++ components/scg/src/scg-math.js
@@ -1,4 +1,4 @@
-SCg.Vector2 = function (x, y) {
+SCg.Vector2 = function (x = 0, y = 0) {
     this.x = x;
     this.y = y;
 };
@@ -93,7 +93,7 @@ SCg.Vector2.prototype = {
 
 
 // --------------------
-SCg.Vector3 = function (x, y, z) {
+SCg.Vector3 = function (x = 0, y = 0, z = 0) {
     this.x = x;
     this.y = y;
     this.z = z;
